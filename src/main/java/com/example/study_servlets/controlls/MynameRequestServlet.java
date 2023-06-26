bf5e7cfc036f3c3fd34ffe24e10b692e4dd5af0c
HeesooHan <hhs950824@gmail.com>
2023-06-26 10:37:27 +0900

    2023-03-26

--- src/main/java/com/example/study_servlets/controlls/MynameRequestServlet.java
+++ src/main/java/com/example/study_servlets/controlls/MynameRequestServlet.java
@@ -4,20 +4,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/MynameRequestServlet")
+@WebServlet(urlPatterns = "/mynameRequestServlet")
 public class MynameRequestServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             PrintWriter printWriter = response.getWriter();
-
             String contents = "<!DOCTYPE html>\r\n" + //
                     "<html lang=\"en\">\r\n" + //
                     "\r\n" + //
@@ -37,16 +34,14 @@ public class MynameRequestServlet extends HttpServlet {
                     "            second name : <input type=\"text\" name=\"second_name\">\r\n" + //
                     "        </div>\r\n" + //
                     "        <div>\r\n" + //
-                    "            <button type=\"submit\"> \uBCF4\uB0B4\uAE30 </button>\r\n" + //
+                    "            <button type=\"submit\">\uBCF4\uB0B4\uAE30</button>\r\n" + //
                     "        </div>\r\n" + //
                     "    </form>\r\n" + //
                     "</body>\r\n" + //
                     "\r\n" + //
                     "</html>";
-
             printWriter.println(contents);
             printWriter.close();
-
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
