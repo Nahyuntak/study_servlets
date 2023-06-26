5148fe5c65a139b234a8aa34091876a0912a90b7
Nahyuntak <hyuntak@snu.ac.kr>
2023-06-26 12:22:42 +0900

    commit

--- src/main/java/com/example/study_servlets/HelloWorldServlet.java
+++ src/main/java/com/example/study_servlets/HelloWorldServlet.java
@@ -15,23 +15,25 @@ public class HelloWorldServlet extends HttpServlet {
             throws ServletException, IOException {
         System.out.println("HelloWorldServlet - doGet()");
         try {
-            String firstName = "yoju";
-            String secondName = "Lab";
+            response.setContentType("text/html;charset=UTF-8");
+            // String firstName = "yoju";
+            // String secondName = "Lab";
             PrintWriter printWriter = response.getWriter();
-            String contents = "<!DOCTYPE html>\r\n" + //
-                    "<html lang=\"en\">\r\n" + //
-                    "<head>\r\n" + //
-                    "    <meta charset=\"UTF-8\">\r\n" + //
-                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
-                    "    <title>Hello World !</title>\r\n" + //
-                    "</head>\r\n" + //
-                    "<body>\r\n" + //
-                    "    <div>\r\n" + //
-                    "        first Name : "+firstName+" </div>\r\n" + //
-                    "    <div>\r\n" + //
-                    "        second Name : "+secondName+" </div>\r\n" + //
-                    "</body>\r\n" + //
-                    "</html>";
+            String contents = "Yoju Lab !";
+            // String contents = "<!DOCTYPE html>\r\n" + //
+            //         "<html lang=\"en\">\r\n" + //
+            //         "<head>\r\n" + //
+            //         "    <meta charset=\"UTF-8\">\r\n" + //
+            //         "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+            //         "    <title>Hello World !</title>\r\n" + //
+            //         "</head>\r\n" + //
+            //         "<body>\r\n" + //
+            //         "    <div>\r\n" + //
+            //         "        first Name : "+firstName+" </div>\r\n" + //
+            //         "    <div>\r\n" + //
+            //         "        second Name : "+secondName+" </div>\r\n" + //
+            //         "</body>\r\n" + //
+            //         "</html>";
 
             printWriter.println(contents);
             printWriter.close();
